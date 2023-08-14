b5d25ab0615f4c3c6acc370c743fbb9867f99028
Sam Storment <sjstorment22@gmail.com>
2023-08-14 09:59:55 -0500

    Fix highlight padding bug

--- src/routes/item/[id=int]/Comment.svelte
+++ src/routes/item/[id=int]/Comment.svelte
@@ -90,114 +90,115 @@
     <article id="{comment.id}">
         <div 
             class="pb-6"
-            class:highlighted
             style={comment.level > 0 ? `margin-left: ${comment.level}rem;` : ""}
         >
-            <div class="text-zinc-600 dark:text-zinc-500 flex justify-between max-sm:flex-col">
-                <div class="mb-1 flex items-center gap-2 mr-2 min-w-0">
-                    <button 
-                        class="flex items-center justify-center font-mono rounded-full shrink-0 group relative"
-                        class:text-black={comment.level % 8 === 0}
-                        class:dark:text-white={comment.level % 8 === 0}
-                        class:text-red-700={comment.level % 8 === 1}
-                        class:text-orange-600={comment.level % 8 === 2}
-                        class:text-yellow-400={comment.level % 8 === 3}
-                        class:text-green-500={comment.level % 8 === 4}
-                        class:text-blue-500={comment.level % 8 === 5}
-                        class:text-indigo-700={comment.level % 8 === 6}
-                        class:text-violet-600={comment.level % 8 === 7}
-                        aria-describedby="copy-{comment.id}"
-                        on:click={copyLink}
-                        on:focusout={() => copied = false}
-                    >
-                        <!-- <iconify-icon icon="fa-solid:link"></iconify-icon> -->
-                        <iconify-icon icon="ph:link-bold"></iconify-icon>
+            <div class:highlighted>
+                <div class="text-zinc-600 dark:text-zinc-500 flex justify-between max-sm:flex-col">
+                    <div class="mb-1 flex items-center gap-2 mr-2 min-w-0">
+                        <button 
+                            class="flex items-center justify-center font-mono rounded-full shrink-0 group relative"
+                            class:text-black={comment.level % 8 === 0}
+                            class:dark:text-white={comment.level % 8 === 0}
+                            class:text-red-700={comment.level % 8 === 1}
+                            class:text-orange-600={comment.level % 8 === 2}
+                            class:text-yellow-400={comment.level % 8 === 3}
+                            class:text-green-500={comment.level % 8 === 4}
+                            class:text-blue-500={comment.level % 8 === 5}
+                            class:text-indigo-700={comment.level % 8 === 6}
+                            class:text-violet-600={comment.level % 8 === 7}
+                            aria-describedby="copy-{comment.id}"
+                            on:click={copyLink}
+                            on:focusout={() => copied = false}
+                        >
+                            <!-- <iconify-icon icon="fa-solid:link"></iconify-icon> -->
+                            <iconify-icon icon="ph:link-bold"></iconify-icon>
+                            <div 
+                                role="tooltip" 
+                                id="copy-{comment.id}" 
+                                class="absolute hidden group-focus-visible:block group-hover:block 
+                                left-full translate-x-1 bg-white dark:bg-black border border-zinc-300 dark:border-zinc-700 
+                                px-2 shadow dark:shadow-zinc-950 rounded text-zinc-600 dark:text-zinc-500 whitespace-nowrap text-sm"
+                                class:copy-tooltip={true}
+                            >
+                                {copied ? "Link Copied!" : "Copy Link"}
+                            </div>
+                        </button>
+                        <span class="whitespace-nowrap overflow-hidden text-ellipsis"><a href="/user/{comment.user}" class:text-blue-500={comment.user === item.user}>{comment.user}</a> <span>{comment.time_ago}</span></span>
+                    </div>
+                        
+                    <div class="flex flex-wrap justify-end lefty:justify-start items-center gap-1 mb-1 text-sm">
+                        <span class="sm:hidden flex-1 h-[22px] border border-zinc-300 dark:border-zinc-700 rounded min-w-0 nothing lefty:hidden"></span>
+                        <!-- <span class="flex-1 h-[1px] bg-zinc-800 min-w-0"></span> -->
+                        {#if root && comment.level > 1}
+                            <button 
+                                type="button" 
+                                class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
+                                on:click={() => scrollTo(root, 'up')}
+                            >
+                                Root
+                            </button>
+                        {/if}
+                        {#if comment.parent}
+                            <button 
+                                type="button" 
+                                class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
+                                on:click={() => scrollTo(comment.parent, 'up')}
+                            >
+                                Parent
+                            </button>
+                        {/if}
+                        {#if prev}
+                            <button 
+                                type="button" 
+                                class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
+                                on:click={() => scrollTo(prev, 'up')}
+                            >
+                                Prev
+                            </button>
+                        {/if}
+                        {#if next}
+                            <button 
+                                type="button" 
+                                class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
+                                on:click={() => scrollTo(next, 'down')}
+                            >
+                                Next
+                            </button>
+                        {/if}
+
+                        <button
+                            type="button" 
+                            class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded font-mono" 
+                            aria-expanded="{visible}"
+                            aria-controls="content-{comment.id}"
+                            on:click={() => visible = !visible}
+                        >
+                            <span class="sr-only">
+                                {visible ? "Collapse Comment" : "Expand Comment"}
+                            </span>
+                            {visible ? "-" : "+"}
+                        </button>
+                    </div>
+                </div>
+
+                {#if visible}
+                    <div id="content-{comment.id}" transition:slide>
                         <div 
-                            role="tooltip" 
-                            id="copy-{comment.id}" 
-                            class="absolute hidden group-focus-visible:block group-hover:block 
-                            left-full translate-x-1 bg-white dark:bg-black border border-zinc-300 dark:border-zinc-700 
-                            px-2 shadow dark:shadow-zinc-950 rounded text-zinc-600 dark:text-zinc-500 whitespace-nowrap text-sm"
-                            class:copy-tooltip={true}
+                            class="prose text-inherit prose-a:dark:text-zinc-500 prose-pre:dark:bg-zinc-950 rounded border border-zinc-300 dark:border-zinc-700 p-2 max-w-full break-words"
                         >
-                            {copied ? "Link Copied!" : "Copy Link"}
+                            {@html comment.content}
                         </div>
-                    </button>
-                    <span class="whitespace-nowrap overflow-hidden text-ellipsis"><a href="/user/{comment.user}" class:text-blue-500={comment.user === item.user}>{comment.user}</a> <span>{comment.time_ago}</span></span>
-                </div>
-                    
-                <div class="flex flex-wrap justify-end lefty:justify-start items-center gap-1 mb-1 text-sm">
-                    <span class="sm:hidden flex-1 h-[22px] border border-zinc-300 dark:border-zinc-700 rounded min-w-0 nothing lefty:hidden"></span>
-                    <!-- <span class="flex-1 h-[1px] bg-zinc-800 min-w-0"></span> -->
-                    {#if root && comment.level > 1}
-                        <button 
-                            type="button" 
-                            class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
-                            on:click={() => scrollTo(root, 'up')}
-                        >
-                            Root
-                        </button>
-                    {/if}
-                    {#if comment.parent}
-                        <button 
-                            type="button" 
-                            class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
-                            on:click={() => scrollTo(comment.parent, 'up')}
-                        >
-                            Parent
-                        </button>
-                    {/if}
-                    {#if prev}
-                        <button 
-                            type="button" 
-                            class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
-                            on:click={() => scrollTo(prev, 'up')}
-                        >
-                            Prev
-                        </button>
-                    {/if}
-                    {#if next}
-                        <button 
-                            type="button" 
-                            class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded" 
-                            on:click={() => scrollTo(next, 'down')}
-                        >
-                            Next
-                        </button>
-                    {/if}
 
-                    <button
-                        type="button" 
-                        class="flex items-center px-2 border border-zinc-300 dark:border-zinc-700 rounded font-mono" 
-                        aria-expanded="{visible}"
-                        aria-controls="content-{comment.id}"
-                        on:click={() => visible = !visible}
-                    >
-                        <span class="sr-only">
-                            {visible ? "Collapse Comment" : "Expand Comment"}
-                        </span>
-                        {visible ? "-" : "+"}
-                    </button>
-                </div>
+                        <div 
+                            class="text-zinc-600 dark:text-zinc-500 text-sm flex items-center gap-2 mt-1" 
+                        >
+                            <iconify-icon icon="cib:y-combinator" class="text-lg"></iconify-icon>
+                            <a href="https://news.ycombinator.com/reply?id={comment.id}&goto=item?id={item.id}#{comment.id}">Reply</a>
+                            <a href="https://news.ycombinator.com/item?id={item.id}#{comment.id}">View</a>
+                        </div>
+                    </div>
+                {/if}
             </div>
-
-            {#if visible}
-                <div id="content-{comment.id}" transition:slide>
-                    <div 
-                        class="prose text-inherit prose-a:dark:text-zinc-500 prose-pre:dark:bg-zinc-950 rounded border border-zinc-300 dark:border-zinc-700 p-2 max-w-full break-words"
-                    >
-                        {@html comment.content}
-                    </div>
-
-                    <div 
-                        class="text-zinc-600 dark:text-zinc-500 text-sm flex items-center gap-2 mt-1" 
-                    >
-                        <iconify-icon icon="cib:y-combinator" class="text-lg"></iconify-icon>
-                        <a href="https://news.ycombinator.com/reply?id={comment.id}&goto=item?id={item.id}#{comment.id}">Reply</a>
-                        <a href="https://news.ycombinator.com/item?id={item.id}#{comment.id}">View</a>
-                    </div>
-                </div>
-            {/if}
         </div>
 
         {#if comment.comments.length > 0}
