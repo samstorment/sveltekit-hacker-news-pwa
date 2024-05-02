724f52c15195d84d198e5aa40cf9fc3f6ade46b1
Sam Storment <sjstorment22@gmail.com>
2024-05-02 16:12:13 -0500

    Fix another oopsy from my overzealous rampage

--- src/lib/components/menu/aside/aside-button.svelte
+++ src/lib/components/menu/aside/aside-button.svelte
@@ -12,7 +12,8 @@
 
 
 <button 
-    class="p-4 sm:py-2 relative w-full flex items-center gap-2 hover:bg-zinc-100 hover:dark:bg-zinc-800"
+    class="p-4 sm:py-2 relative w-full flex items-center gap-2 hover:bg-zinc-100 hover:dark:bg-zinc-800 
+    after:bg-blue-500 after:dark:bg-zinc-200"
     class:active={$activeTab === tab}
     on:click={() => activate(tab)}
 >
@@ -20,3 +21,14 @@
     <iconify-icon {icon} class="text-2xl"></iconify-icon>
     <span class="max-sm:sr-only sm:min-w-[100px] text-left capitalize">{text}</span>
 </button>
+
+<style>
+    .active::after {
+        position: absolute;
+        display: block;
+        height: 100%;
+        width: 3px;
+        right: 0;
+        top: 0;
+    }
+</style>
